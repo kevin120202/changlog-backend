88fd632ae57971e08885db724df5a650dcfd6b58
Kevin Dela Paz <kevindelapaz21@gmail.com>
2024-09-09 18:36:06 -0500

    added input validation

--- src/router.ts
+++ src/router.ts
@@ -1,28 +1,47 @@
 import { Router } from "express";
-const router = Router()
+import { body, oneOf } from "express-validator"
+import { handleInputErrors } from "./modules/middleware";
 
+const router = Router()
 
 // Product
 router.get("/product", (req, res) => {
     res.status(200).json("product")
 })
 router.get("/product/:id", () => { })
-router.put("/product/:id", () => { })
-router.post("/product", () => { })
+router.put("/product/:id", body("name").isString().withMessage("Name is required"), handleInputErrors, (req, res) => {
+
+})
+router.post("/product", body("name").isString().withMessage("Name is required"), handleInputErrors, () => { })
 router.delete("/product/:id", () => { })
 
 // Update
 router.get("/update", () => { })
 router.get("/update/:id", () => { })
-router.put("/update/:id", () => { })
-router.post("/update", () => { })
+router.put("/update/:id",
+    body('title').exists().isString(),
+    body('body').exists().isString(),
+    body('status').isIn(['IN_PROGRESS', 'SHIPPED', 'DEPRECATED']).optional(),
+    body('version').optional(),
+    () => { })
+router.post("/update",
+    body('title').exists().isString(),
+    body('body').exists().isString(),
+    () => { })
 router.delete("/update/:id", () => { })
 
 // Update Point
 router.get("/update-point", () => { })
 router.get("/update-point/:id", () => { })
-router.put("/update-point/:id", () => { })
-router.post("/update-point", () => { })
+router.put("/update-point/:id",
+    body('name').optional().isString(),
+    body('description').optional().isString(),
+    () => { })
+router.post("/update-point",
+    body('name').isString(),
+    body('description').isString(),
+    body('updateId').exists().isString(),
+    () => { })
 router.delete("/update-point/:id", () => { })
 
 
--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,8 @@ import morgan from "morgan"
 import cors from "cors"
 import { protect } from "./modules/auth"
 import { login, register } from "./handlers/user"
+import { check, body } from "express-validator";
+import { handleInputErrors } from "./modules/middleware"
 
 const app = express()
 
@@ -13,7 +15,17 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.use("/api", protect, router)
-app.post("/user", register)
-app.post("/login", login)
+
+app.post("/user",
+    [body('username').isLength({ min: 3 }).withMessage('Username must be at least 3 characters long'),
+    body('password')
+        .isLength({ min: 6 }).withMessage('Password must be at least 6 characters long')
+    ], handleInputErrors,
+    register)
+
+app.post("/login",
+    [body('email').isEmail().withMessage('Must be a valid email address'),
+    body('password').notEmpty().withMessage('Password is required')], handleInputErrors,
+    login)
 
 export default app
